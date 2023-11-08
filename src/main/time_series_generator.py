d696281fcf98092d0b37cf38e8814647fb2b92a8
martilut <jeykolton@gmail.com>
2023-11-08 16:38:47 +0300

    fixed time series generator init

--- src/main/time_series_generator.py
+++ src/main/time_series_generator.py
@@ -2,7 +2,8 @@ from schedule import Schedule
 
 
 class TimeSeriesGenerator:
-    def __init__(self, num_steps, process_id):
+    def __init__(self, num_time_series, num_steps, process_id):
+        self.num_time_series = num_time_series
         self.num_steps = num_steps
         self.process_id = process_id
 
