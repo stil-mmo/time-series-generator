b377320fd93f9b4f5b01030bf3c6ff00cd6be159
martilut <jeykolton@gmail.com>
2023-11-29 16:09:24 +0300

    added point clustering

---
+++ src/main/scheduler_storage.py
@@ -0,0 +1,30 @@
+from numpy import float32
+from numpy.typing import NDArray
+
+from src.main.generator_linspace import GeneratorLinspace
+from src.main.scheduler import Scheduler
+
+
+class SchedulerStorage:
+    def __init__(self, num_steps: int, generator_linspace: GeneratorLinspace, points: NDArray[float32], clusters: NDArray[int]):
+        self.num_steps = num_steps
+        self.generator_linspace = generator_linspace
+        self.points = points
+        self.clusters = clusters
+        self.scheduler_storage = self.create_storage()
+
+    def create_storage(self) -> dict[int, Scheduler]:
+        storage = {}
+        for cluster in self.clusters:
+            if cluster not in storage.keys():
+                scheduler = Scheduler(self.num_steps, self.generator_linspace)
+                storage[cluster] = scheduler
+        return storage
+
+    def get_cluster(self, point_index: int) -> int:
+        return self.clusters[point_index]
+
+    def get_scheduler(self, cluster: int, source_values: NDArray[float] | None = None) -> Scheduler:
+        if source_values is not None:
+            self.scheduler_storage[cluster].set_source_values(source_values)
+        return self.scheduler_storage[cluster]
--- src/main/time_series_generator.py
+++ src/main/time_series_generator.py
@@ -1,10 +1,12 @@
-from generator_linspace import GeneratorLinspace
 from numpy import ndarray
-from numpy.typing import NDArray
+
+from generator_linspace import GeneratorLinspace
 from scheduler import Scheduler
 from src.main.generator_typing import ProcessDataType, ProcessOrderType
-from src.main.point_sampling import sample_points
+from src.main.point_clustering import cluster_points
+from src.main.point_sampling import sample_points, move_points
 from src.main.process_list import ProcessList
+from src.main.scheduler_storage import SchedulerStorage
 from src.main.time_series import TimeSeries
 from src.main.utils.utils import show_plot
 
@@ -14,15 +16,35 @@ class TimeSeriesGenerator:
         self,
         num_time_series: int,
         num_steps: int,
+        generator_linspace: GeneratorLinspace,
+        scheduler_storage: SchedulerStorage,
         process_list: ProcessList | None = None,
         process_order: ProcessOrderType | None = None,
-        points: NDArray | None = None,
+        stable_parameters: bool = True,
+        single_schedule: bool = True,
     ):
         self.num_time_series = num_time_series
         self.num_steps = num_steps
+        self.generator_linspace = generator_linspace
+        self.scheduler_storage = scheduler_storage
         self.process_list = process_list
         self.process_order = process_order
-        self.points = points
+        self.stable_parameters = stable_parameters
+        self.single_schedule = single_schedule
+
+    def generate_new_scheduler(self) -> Scheduler:
+        return Scheduler(
+            num_steps=self.num_steps,
+            generator_linspace=self.generator_linspace,
+            process_list=self.process_list,
+            process_order=self.process_order,
+        )
+
+    def get_point_schedule(self, point_index: int) -> tuple[ProcessList, list[ProcessDataType]]:
+        cluster = self.scheduler_storage.get_cluster(point_index)
+        scheduler = self.scheduler_storage.get_scheduler(cluster)
+        scheduler.set_source_values(self.scheduler_storage.points[point_index])
+        return scheduler.process_list, scheduler.generate_schedule(self.stable_parameters)
 
     def generate_time_series(
         self,
@@ -48,66 +70,61 @@ class TimeSeriesGenerator:
                     )
         return current_time_series
 
-    def generate_all(
-        self,
-        generator_linspace: GeneratorLinspace,
-        stable_parameters: bool = True,
-        single_schedule: bool = True,
-    ) -> tuple[ndarray, list[TimeSeries]]:
+    def generate_all(self, ) -> tuple[ndarray, list[TimeSeries]]:
         ts_array = ndarray((self.num_time_series, self.num_steps))
         ts_list = []
-        scheduler = Scheduler(
-            num_steps=self.num_steps,
-            generator_linspace=generator_linspace,
-            process_list=self.process_list,
-            process_order=self.process_order,
-        )
-        if self.points is not None:
-            scheduler.set_source_values(self.points[0])
-        schedule = (
-            None
-            if not single_schedule
-            else scheduler.generate_schedule(stable_parameters=stable_parameters)
-        )
+        scheduler = self.generate_new_scheduler()
         for i in range(self.num_time_series):
-            if schedule is None:
-                if self.points is None:
+            if self.scheduler_storage is None:
+                if self.single_schedule:
                     ts = self.generate_time_series(
                         scheduler.process_list,
-                        scheduler.generate_schedule(
-                            stable_parameters=stable_parameters
-                        ),
+                        scheduler.generate_schedule(stable_parameters=self.stable_parameters),
                     )
-                    ts_array[i] = ts.get_values()
-                    ts_list.append(ts)
                 else:
-                    scheduler.set_source_values(self.points[i])
+                    scheduler = self.generate_new_scheduler()
                     ts = self.generate_time_series(
                         scheduler.process_list,
                         scheduler.generate_schedule(
-                            stable_parameters=stable_parameters
+                            stable_parameters=self.stable_parameters
                         ),
                     )
-                    ts_array[i] = ts.get_values()
-                    ts_list.append(ts)
+                ts_array[i] = ts.get_values()
+                ts_list.append(ts)
             else:
-                ts = self.generate_time_series(scheduler.process_list, schedule)
+                process_list, schedule = self.get_point_schedule(i)
+                ts = self.generate_time_series(
+                    process_list,
+                    schedule,
+                )
                 ts_array[i] = ts.get_values()
                 ts_list.append(ts)
         return ts_array, ts_list
 
 
 if __name__ == "__main__":
-    coordinates, border_values = sample_points(3)
-    ts_generator = TimeSeriesGenerator(
-        num_time_series=3, num_steps=100, points=coordinates
-    )
+    coordinates, border_values = sample_points(5)
+    coordinates = move_points(coordinates)
+    print(coordinates)
+    clusters = cluster_points(coordinates, 4)
+    print(f"Clusters: {clusters}")
     test_generator_linspace = GeneratorLinspace(
         start=border_values[0], stop=border_values[1], parts=100
     )
-    time_series_array, time_series_list = ts_generator.generate_all(
+    storage = SchedulerStorage(
+        num_steps=100,
         generator_linspace=test_generator_linspace,
+        points=coordinates,
+        clusters=clusters,
+    )
+    ts_generator = TimeSeriesGenerator(
+        num_time_series=coordinates.shape[0],
+        num_steps=100,
+        generator_linspace=test_generator_linspace,
+        scheduler_storage=storage,
         stable_parameters=False,
         single_schedule=False,
     )
+
+    time_series_array, time_series_list = ts_generator.generate_all()
     show_plot(time_series_array)
